2a9a1b534084d6407e12972b39edd5b8b334f79a
Anders Tunold Kråkenes & Elisa Verna & Terence Stone <pair+anderskraken.elisuh.terencestone@devbootcamp.com>
2016-08-31 12:06:47 -0400

    changed migration to account for typo

--- db/migrate/20160831110059_create_answers.rb
+++ db/migrate/20160831110059_create_answers.rb
@@ -1,6 +1,6 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
-    create_table :questions do |t|
+    create_table :answers do |t|
       t.text    :body, null: false
       t.integer :user_id, null: false
       t.integer :question_id, null: false
