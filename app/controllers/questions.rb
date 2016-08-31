3038d53c15d0e7f9df2304c492fa9121520b5843
Anders Tunold Kråkenes & Elisa Verna & Terence Stone <pair+anderskraken.elisuh.terencestone@devbootcamp.com>
2016-08-31 15:32:12 -0400

    created routes and views for questions, completed new question form, question index page and tags helper method

---
+++ app/controllers/questions.rb
@@ -0,0 +1,27 @@
+get '/questions' do
+  @questions = Question.all
+  erb :'questions/index'
+end
+
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+    question = Question.new(params[:question])
+  if question.save
+    tag_array = params[:tags].split(' ')
+    create_tags(tag_array)
+    redirect '/questions'
+  else
+    @errors = question.errors.full_messages
+    erb :'questions/new'
+  end
+end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  @user = @question.user
+  @answers = @question.answers
+  erb :'questions/show'
+end
