f323d9a628d34027c7544bcdb0d8cbfcf71f5439
Terence Stone <terence.stone1@gmail.com>
2016-08-31 18:12:19 -0400

    Complete voting feature for answers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,3 +7,14 @@ post '/questions/:id/vote' do
   end
   redirect "/questions/#{question.id}"
 end
+
+post '/answers/:id/vote' do
+  answer = Answer.find(params[:id])
+  question = answer.question
+  if params[:vote] == "upvote"
+    answer.votes.create(value: 1, user_id: current_user.id )
+  else
+    answer.votes.create(value: -1, user_id: current_user.id )
+  end
+  redirect "/questions/#{question.id}"
+end
