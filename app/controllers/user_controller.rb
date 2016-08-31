68c3b442feb2f341ac215e7b88c5dff5556051e6
Terence Stone <terence.stone1@gmail.com>
2016-08-31 15:19:24 -0400

    Complete user registration, log in, and log out feature

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,15 +13,22 @@ post '/users' do
   end
 end
 
-get 'users/login' do
-
+get '/users/login' do
+  erb :'users/login'
 end
 
-post 'users/login' do
-
+post '/users/login' do
+  user = User.find_by(username: params[:username])
+  if user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @error = "Invalid username or password"
+    erb :'users/login'
+  end 
 end
 
-get 'users/logout' do
+get '/users/logout' do
   session.delete(:user_id)
   redirect '/'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,8 @@ require "sinatra/reloader" if development?
 require 'erb'
 require 'securerandom'
 
+require 'bcrypt'
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
