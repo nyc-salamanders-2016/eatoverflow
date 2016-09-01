f54fde932376f07dfc791f393b10e5fda0ab729c
Elisa Verna <email": null>
2016-09-01 17:59:00 -0400

    fixed merge conflict

--- public/js/application.js
+++ public/js/application.js
@@ -2,7 +2,13 @@
 $(document).ready(function() {
   $('#question_container').on('submit', '#comment_question_form', function(event){
     event.preventDefault();
+    $.ajax({
+      method: $(event.target).attr('method'),
+      url: ,
+      data:
+    }).done(function(response){
 
+    })
 
   })
 });
