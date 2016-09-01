565a18f05ef447b270dcfa59f46b1cd872cd4fae
Terence Stone <terence.stone1@gmail.com>
2016-09-01 13:03:00 -0400

    Complete feature for questions

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,11 +1,28 @@
 post '/questions/:id/vote' do
-  question = Question.find(params[:id])
-  if params[:vote] == "upvote"
-    question.votes.create(value: 1, user_id: current_user.id )
+  @question = Question.find(params[:id])
+  @answers = @question.answers
+  @score = get_score(@question)
+
+  if params[:vote] == "upvote" 
+   upvote = @question.votes.new(value: 1, user_id: current_user.id )
+    if upvote.authenticate_vote(current_user.id, @question.id) == false
+      upvote.save
+      redirect "/questions/#{@question.id}"
+    else
+      @error = "You can only vote once"
+      erb :"questions/show"
+    end
   else
-    question.votes.create(value: -1, user_id: current_user.id )
+    downvote = @question.votes.new(value: -1, user_id: current_user.id )
+    if downvote.authenticate_vote(current_user.id, @question.id) == false
+      downvote.save
+      redirect "/questions/#{@question.id}"
+    else
+      @error = "You can only vote once"
+      erb :"questions/show"
+    end
   end
-  redirect "/questions/#{question.id}"
+  # redirect "/questions/#{@question.id}"
 end
 
 post '/answers/:id/vote' do
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,11 @@
 class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
   belongs_to :user
+
+  # validates :user, uniqueness: { scope: :voteable, message: "you can only vote once." } 
+
+  def authenticate_vote(user_id, item_id)
+    # user = User.find(self.user_id)
+    !!user.votes.find_by(voteable_id: item_id )
+  end
 end
