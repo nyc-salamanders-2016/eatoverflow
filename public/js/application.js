4b1e421ed76a5eb6b4f42d7e89caf8df1154af8f
Elisa Verna <email": null>
2016-09-01 17:57:56 -0400

    fixed merge conflict

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,10 @@
+// ajax for comments on questions
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('#question_container').on('submit', '#comment_question_form', function(event){
+    event.preventDefault();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  })
 });
+
+// ajax for comments on answers
