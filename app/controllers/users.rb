7f6db3847b6b47f722832feb05837881c3525643
Elisa Verna <email": null>
2016-09-01 11:52:53 -0400

    created profile route and show, added questions asked and questions answered to user profile

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -32,3 +32,10 @@ get '/users/logout' do
   session.clear
   redirect '/'
 end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  @user_questions = @user.questions
+  @user_answers = @user.answers
+  erb :'users/show'
+end
