d1cfadfbc65a115a8fabb7088f8c188fd299ad3d
Elisa Verna <email": null>
2016-09-01 11:35:41 -0400

    added question-tags table, show tags in question view, link individual tags to page with all questions tagged with that tag

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,8 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :votes, as: :voteable
   has_many :comments, as: :commentable
-  has_many :tags
+  has_many :question_tags
+  has_many :tags, through: :question_tags
 
   validates :title, :body, presence: true
   validates :title, length: { minimum: 2 }
---
+++ db/migrate/20160901112120_question_tags.rb
@@ -0,0 +1,10 @@
+class QuestionTags < ActiveRecord::Migration
+def change
+    create_table :question_tags do |t|
+      t.integer :question_id, null: false
+      t.integer :tag_id, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
