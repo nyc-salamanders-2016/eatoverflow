4a3f2463732bac5af02bb10a68a48a65203d03d4
Terence Stone <terence.stone1@gmail.com>
2016-09-01 14:18:19 -0400

    Add logic to answer vote buttons

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -24,11 +24,32 @@ end
 
 post '/answers/:id/vote' do
   answer = Answer.find(params[:id])
-  question = answer.question
-  if params[:vote] == "upvote"
-    answer.votes.create(value: 1, user_id: current_user.id )
-  else
-    answer.votes.create(value: -1, user_id: current_user.id )
+  @question = answer.question
+  @answers = @question.answers
+  @score = get_score(@question)
+
+   if params[:vote] == "upvote" 
+    vote = answer.votes.new(value: 1, user_id: current_user.id )
+    if vote.save
+      redirect "/questions/#{@question.id}"
+    else
+      @errors = vote.errors.full_messages
+      erb :"questions/show"
+    end
+  else params[:vote] == "downvote"
+    vote = answer.votes.new(value: -1, user_id: current_user.id )
+    if vote.save
+      redirect "/questions/#{@question.id}"
+    else
+      @errors = vote.errors.full_messages
+      erb :"questions/show"
+    end
   end
-  redirect "/questions/#{question.id}"
 end
+#   if params[:vote] == "upvote"
+#     answer.votes.create(value: 1, user_id: current_user.id )
+#   else
+#     answer.votes.create(value: -1, user_id: current_user.id )
+#   end
+#   redirect "/questions/#{question.id}"
+# end
