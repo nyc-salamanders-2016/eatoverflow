a520a743fa4b2d9a8a9b80b3a11afa7a026dc5fb
[01;31m[K"name"[m[K: "Anders Tunold Kråkenes <[01;31m[K"email"[m[K: "tunold@gmail.com>
2016-09-02 03:38:59 -0400

    Add class tags for bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,16 +17,20 @@ question4 = Question.create( title: "Did you know you can donate one or all of y
 
 question5 = Question.create( title: "What is tea time?", body: "This is a body", user_id: 1 )
 
+question4 = Question.create( title: "What is Snorlax?", body: "It's true!", user_id: 4 )
+
 answer1 = Answer.create( body: "Man is a being of spirit", user_id: 1, question_id: 1 )
 
 answer2 = Answer.create( body: "A miserable little pile of secrets!", user_id: 2, question_id: 1 )
 
 tag1 = Tag.create( name: "philosophy", question_id: 1 )
 
-tag2 = Tag.create( name: "philosophy", question_id: 2 )
+tag2 = Tag.create( name: "pokemon", question_id: 2 )
 
 comment1 = Question.first.comments.create( body: "This is a comment from seed on a question.", user_id: 1 )
 
 comment2 = Answer.first.comments.create( body: "This is a comment from seed on an answer.", user_id: 1 )
 
-# vote1 = Vote.create( user_id: 3, value: 1, voteable_id: 1 voteable_type: ???? )
+comment3 = Answer.first.comments.create( body: "Enough comments already!", user_id: 4 )
+
+vote1 = Vote.create( user_id: 3, value: 1, voteable_id: 1, voteable_type: "Question" )
